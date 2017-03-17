effb177a07915c05231a7c458215d66e88b27a83
Bob <stan117@rocketmail.com>
2017-03-17 00:57:02 -0700

    loading dumping and multiple connections

--- src/sri/main.cpp
+++ src/sri/main.cpp
@@ -7,6 +7,9 @@
  */
 
 #include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <ctime>
 #include <cstring>
 #include <thread>
 #include "sri.hpp"
@@ -16,45 +19,68 @@
 
 using namespace std;
 
+//used for the dump command
+void sendFile(TCPSocket *client, SRI *s) {
+    char fname[32];
+    //generate a unique name for a temp file
+    sprintf(fname, "~tmp%ld", time(NULL));
+    //dump into that file
+    s->dump(string(fname));
+    //send the file to the client
+    ifstream file;
+    file.open(fname);
+    string line;
+    while(getline(file, line)) {
+        //send the line to the client
+        client->writeToSocket(line.c_str(), line.length());
+    }
+    file.close(); //close and delete the temp file
+    remove(fname);
+    line = "\n"; //end the file
+    client->writeToSocket(line.c_str(), line.length());
+}
+
+//handle a client connection
 void handleConnection(TCPSocket *client) {
     cout << "Started client thread\n";
-    SRI s;
-    char buffer[8192];
+    SRI s; //create a new SRI for this client
+    char buffer[8192]; //buffer for sending data
     
-    while (true) {
-        client->readFromSocket(buffer, 65536);
-        string input(buffer);
-        memset(buffer, 0, 8192);
-        string ret;
-
+    while (true) { //loop until the user types 'q'
+        memset(buffer, 0, 8192); //zero the buffer
+        int recv = client->readFromSocket(buffer, 65536); //read data
+        if (recv < 1) break; //break if the user improperly disconnected
+        string input(buffer); //convert it to a string
+        string ret; //string that is returned to the client
+        
+        //break if the user requested a disconnect
         if (input == "q" || input == "Q") break;
             try
             {
-                ret = s.parseInput(input);
-                if (ret == "") ret = "\n";
+                if (input.substr(0,5) == "DUMP ") {
+                    sendFile(client, &s);
+                    continue;
+                }
+                ret = s.parseInput(input); //the SRI's parse function
+                if (ret == "") ret = "\n"; //return \n for no data
             }
             catch(...)
             {
-                ret = "Error parsing input\n";
+                ret = "Error parsing input\n"; //malformed query
             }
-        strcpy(buffer, ret.c_str());
-        client->writeToSocket(buffer, strlen(buffer));
+        //strcpy(buffer, ret.c_str()); //copy the return string into the output buffer
+        client->writeToSocket(ret.c_str(), ret.length()); //send it off
     }
-    delete client;
+    delete client; //close the socket when the client disconnects
     cout << "Ended client thread\n";
 }
 
-void call_from_thread(string s) {
-    cout << s << "\n";
-}
-
 int main() {
-    bool running = true;
-    TCPServerSocket server(9999);
-    while (running) {
-        TCPSocket * client = server.getConnection();
+    TCPServerSocket server(9999); //start a server
+    while (true) { //loop forever waiting for connections
+        TCPSocket * client = server.getConnection(); //get a connection
         cout << "Recieved Connection" << "\n";
-        thread(handleConnection, client).detach();
+        thread(handleConnection, client).detach(); //spawn a thread to handle it
     }
     
     return 0;
--- src/sri/main2.cpp
+++ src/sri/main2.cpp
@@ -7,30 +7,106 @@
  */
 
 #include <iostream>
+#include <fstream>
 #include <cstring>
 #include "TCPSocket.h"
 
 
 using namespace std;
 
+char buffer[8192]; //buffer for reading data
+
+//called when the user types LOAD
+void sendFile(TCPSocket *client, const char *fname) {
+    ifstream file;
+    file.open(fname); //open the file
+    if (!file.is_open()) {
+        std::cout << "Failed to load from file \'" << fname << "\', consider checking the file name and/or extension.\n";
+        return;
+    }
+
+    string line;
+    while(getline(file, line)) {
+        //don't send a blank line
+        if (line == "") continue;
+        //send a line of the file like a normal command
+        client->writeToSocket(line.c_str(), line.length());
+        //wait for the server to acknowledge
+        client->readFromSocket(buffer, 65536);
+    }
+    file.close();
+}
+
+//called when the user types dump
+void recieveFile(TCPSocket *client, const char *fname) {
+    ofstream file(fname); //open file for writing
+    buffer[0] = 0;
+
+    client->readFromSocket(buffer, 65536);
+    while (buffer[0] != '\n') { //loop until server is done
+        file << buffer << "\n";
+        client->readFromSocket(buffer, 65536);
+    }
+
+    file.close(); //close the file
+}
 
 int main() {
-    char addr[128] = "127.0.0.1";
-    TCPSocket client(addr, 9999);
-    
-    char buffer[8192];
-    string input;
-    
-    while (input != "q" && input != "Q") {
-        input = "";
-        while (input == "") {
-            printf("Enter an SRI comand, or (Q)uit : ");
-            getline(cin, input);
+    TCPSocket *client; //pointer to the socket
+
+    while (true) { //loop, allowing the client to connect to different servers
+        while (true) { //loop until the user provides a valid address
+            char addr[128];
+            cout << "Enter the server IP, or (Q)uit: ";
+            cin.getline(addr, 128);
+
+            //exit if the user typed q
+            if (addr[0] == 'q' || addr[0] == 'Q') return 0;
+            try {
+                client = new TCPSocket(addr, 9999); //attempt to connect
+                break; //leave the loop if successful
+            }
+            catch (...) {
+                //failed, try again
+                cout << "Failed to connect to " << addr <<"\n";
+            }
         }
-        int ret = client.writeToSocket(input.c_str(), input.length());
-        //cout << "Wrote " << ret << " bytes" << "\n";
-        client.readFromSocket(buffer, 65536);
-        if (buffer[0] != '\n') cout << buffer;
+        
+        string input; //the user's input
+        
+        while (input != "q" && input != "Q") { //disconnect if the user typed q
+            input = "";
+            while (input == "") { //don't try to send an empty string
+                printf("Enter an SRI comand, or (Q)uit: ");
+                getline(cin, input); //get user input
+            }
+
+            //clientside handling of load and dump
+            string cmd = input.substr(0,5);
+            if (cmd == "LOAD ") {
+                sendFile(client, input.substr(5).c_str());
+                continue;
+            }
+            if (cmd == "DUMP ") {
+                //tell the server we want a dump
+                client->writeToSocket(input.c_str(), input.length());
+                //handle the response
+                recieveFile(client, input.substr(5).c_str());
+                continue;
+            }
+
+            //send the query to the server
+            client->writeToSocket(input.c_str(), input.length());
+            //wait for a response
+            int recv = client->readFromSocket(buffer, 65536);
+            if (recv < 1) { //break on connection loss
+                cout << "Lost connection to server\n";
+                break;
+            }
+            //output nothing if the server returned \n
+            if (buffer[0] != '\n') cout << buffer;
+        }
+        delete client; //close the socket
     }
     
     return 0;
